c9cd0afb415dbf65bb3aae8f6c7602700362bafb
KaramAmin <karam.pucit@gmail.com>
2023-01-26 20:51:15 +0500

    weight conditions added

--- src/pages/ExtraPages/SignIn.js
+++ src/pages/ExtraPages/SignIn.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  Button,
   Card,
   CardBody,
   Col,
@@ -25,6 +26,9 @@ import { TheTailSpinner } from "../../components/Spinners";
 import { successToast } from "../../components/Toasts";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Icon } from "@iconify/react";
+import Switch from "react-bootstrap/esm/Switch";
+import CheckboxField from "eversign/lib/CheckboxField";
 
 const SignIn = () => {
   const [email, setEmail] = useState();
@@ -111,14 +115,39 @@ const SignIn = () => {
       });
   };
 
-  // useEffect(() => {
-  //   var username = localStorage.getItem("email");
-  //   var password = localStorage.getItem("password");
-  //   if (username != null) {
-  //     window.location.href = "/joblist"; // Assign your url to location href
-  //   }
-  // });
+  const [nmbOfDisplay, setNmbOfDsiplay] = useState([]);
+  const [weightLBS, setWeightLBS] = useState();
+  const [totalWeightLBS, setTotalWeightLBS] = useState();
+  const [addedWeightLBS, setAddedWeightLBS] = useState();
 
+  const handleAddNewRow = (e) => {
+    setNmbOfDsiplay([
+      ...nmbOfDisplay,
+      { Input: " weight (lbs)" },
+      // { Select: " Unit type" },
+      // { Select: " #of Units" },
+      // { Switch: " Stackable" },
+      // { Dimensions: " Dimensions" },
+      // { Dimensions: " Dimensions" },
+      // { Dimensions: " Dimensions" },
+    ]);
+    // setNmbOfDsiplay(nmbOfDisplay + 1)
+    // e.preventDefault();
+    // window.location.reload()
+    // Array.push(
+    //   { title: " weight" },
+    //   { title: " Exact weight" },
+    //   { title: " Dimensions" },
+    //   { title: " Units" }
+    // );
+    // console.log(Array, "ArrayArray");
+  };
+  const handleWeightChange = (e, weight) => {
+    if (weight === "firstW") {
+      setWeightLBS(e.target.value);
+    } else setAddedWeightLBS(e.target.value);
+    setTotalWeightLBS(weightLBS + addedWeightLBS);
+  };
   return (
     <React.Fragment>
       <ToastContainer
@@ -142,143 +171,181 @@ const SignIn = () => {
             </MetaTags>
             <section className="bg-auth">
               <Container>
-                <Row className="justify-content-center">
-                  <Col xl={10} lg={12}>
-                    <Card className="auth-box">
-                      <Row className="g-0">
-                        <Col lg={6} className="text-center">
-                          <CardBody className="p-4">
-                            <Link to="/">
-                              <img
-                                src={lightLogo}
-                                alt=""
-                                className="logo-light"
-                              />
-                              <img
-                                src={darkLogo}
-                                alt=""
-                                className="logo-dark"
-                              />
-                            </Link>
-                            <div className="mt-5">
-                              <img
-                                src={signInImage}
-                                alt=""
-                                className="img-fluid"
-                              />
+                <Row>
+                  <Col lg={4}>
+                    <div style={{ display: "flex" }}>
+                      <div>
+                        <svg
+                          onClick={() => handleAddNewRow()}
+                          style={{
+                            width: 50,
+                            cursor: "pointer",
+                            marginTop: 30,
+                          }}
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-plus-circle-fill"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+                        </svg>
+                      </div>
+                      <div>
+                        <label>Weight</label>
+                        <br />
+                        <Input
+                          placeholder="Weight"
+                          value={weightLBS}
+                          onChange={(e) => handleWeightChange(e, "firstW")}
+                        ></Input>
+                      </div>
+                      <div>
+                        <label>unit type</label>
+                        <br />
+                        <Input placeholder="Unit type"></Input>
+                      </div>
+                    </div>
+                  </Col>
+                  <Col lg={4}>
+                    <div style={{ display: "flex" }}>
+                      <div>
+                        <label># of Units</label>
+                        <br />
+                        <Input placeholder="# of Units"></Input>
+                      </div>
+                      <div>
+                        <br />
+                        <br />
+
+                        {/* <br /> */}
+                        <input
+                          class="form-check-input"
+                          type="checkbox"
+                          value=""
+                          id="flexCheckChecked"
+                          // checked
+                        />
+                        <label class="form-check-label" for="flexCheckChecked">
+                          Stackable
+                        </label>
+                      </div>
+                    </div>
+                  </Col>
+                  <Col lg={4}>
+                    <label>Dimensions</label>
+                    <div style={{ display: "flex" }}>
+                      <div>
+                        <Input placeholder="Length"></Input>
+                      </div>
+                      <div>
+                        <Input placeholder="Width"></Input>
+                      </div>
+                      <div>
+                        <Input placeholder="Height"></Input>
+                      </div>
+                      {/* <br/> */}
+                    </div>
+                    <small>
+                      <u>
+                        <b>total Weight (lbs){weightLBS}</b>
+                      </u>
+                    </small>
+                  </Col>
+                </Row>
+                <Row>
+                  {nmbOfDisplay
+                    // .filter((items) => items.Input)
+                    .map((items) => (
+                      <Row>
+                        <Col lg={4}>
+                          <div style={{ display: "flex" }}>
+                            <div>
+                              <svg
+                                onClick={() => handleAddNewRow()}
+                                style={{
+                                  width: 50,
+                                  cursor: "pointer",
+                                  marginTop: 30,
+                                }}
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                fill="currentColor"
+                                class="bi bi-plus-circle-fill"
+                                viewBox="0 0 16 16"
+                              >
+                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+                              </svg>
                             </div>
-                          </CardBody>
+                            <div>
+                              <label>Weight</label>
+                              <br />
+                              <Input
+                                placeholder="Weight"
+                                value={addedWeightLBS}
+                                onChange={(e) =>
+                                  handleWeightChange(e, "addedW")
+                                }
+                              ></Input>
+                            </div>
+                            <div>
+                              <label>unit type</label>
+                              <br />
+                              <Input placeholder="Unit type"></Input>
+                            </div>
+                          </div>
                         </Col>
-                        <Col lg={6}>
-                          <CardBody className="auth-content p-5 h-100 text-white">
-                            <div className="w-100">
-                              <div className="text-center mb-4">
-                                <h5>Welcome Back !</h5>
-                                <p className="text-white-70">
-                                  Sign in to continue to Jobcy.
-                                </p>
-                              </div>
-                              <Form className="auth-form">
-                                <div className="mb-3">
-                                  <label
-                                    htmlFor="usernameInput"
-                                    className="form-label"
-                                  >
-                                    Username
-                                  </label>
-                                  <Input
-                                    type="text"
-                                    className="form-control"
-                                    id="usernameInput"
-                                    placeholder="Enter your username"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    required
-                                  />
-                                </div>
-                                <div className="mb-3">
-                                  <label
-                                    htmlFor="passwordInput"
-                                    className="form-label"
-                                  >
-                                    Password
-                                  </label>
-                                  <Input
-                                    type="password"
-                                    className="form-control"
-                                    id="passwordInput"
-                                    placeholder="Enter your password"
-                                    value={password}
-                                    onChange={(e) =>
-                                      setPassword(e.target.value)
-                                    }
-                                    required
-                                  />
-                                </div>
-                                <div className="mb-4">
-                                  <div className="form-check">
-                                    <input
-                                      className="form-check-input"
-                                      type="checkbox"
-                                      id="flexCheckDefault"
-                                    />
-                                    <Link
-                                      to="/resetpassword"
-                                      className="float-end text-white"
-                                    >
-                                      Forgot Password?
-                                    </Link>
-                                    <label
-                                      className="form-check-label"
-                                      htmlFor="flexCheckDefault"
-                                    >
-                                      Remember me
-                                    </label>
-                                  </div>
-                                </div>
-                                <div className="text-center">
-                                  <button
-                                    type="submit"
-                                    className="btn btn-white btn-hover w-100"
-                                    onClick={handleSubmit}
-                                  >
-                                    <div>
-                                      <span style={{ display: "inline-block" }}>
-                                        Sign In
-                                      </span>
-                                      <span
-                                        style={{
-                                          display: "inline-block",
-                                          marginLeft: "50px",
-                                        }}
-                                      >
-                                        <TheTailSpinner
-                                          isLoading={isLoading}
-                                          width={23}
-                                          height={23}
-                                        />
-                                      </span>
-                                    </div>
-                                  </button>
-                                </div>
-                              </Form>
-                              <div className="mt-4 text-center">
-                                <p className="mb-0">
-                                  Don't have an account ?
-                                  <Link
-                                    to="/signup"
-                                    className="fw-medium text-white text-decoration-underline"
-                                  >
-                                    Sign Up
-                                  </Link>
-                                </p>
-                              </div>
+                        <Col lg={4}>
+                          <div style={{ display: "flex" }}>
+                            <div>
+                              <label># of Units</label>
+                              <br />
+                              <Input placeholder="# of Units"></Input>
                             </div>
-                          </CardBody>
+                            <div>
+                              <br />
+                              <br />
+
+                              {/* <br /> */}
+                              <input
+                                class="form-check-input"
+                                type="checkbox"
+                                value=""
+                                id="flexCheckChecked"
+                                // checked
+                              />
+                              <label
+                                class="form-check-label"
+                                for="flexCheckChecked"
+                              >
+                                Stackable
+                              </label>
+                            </div>
+                          </div>
+                        </Col>
+                        <Col lg={4}>
+                          <label>Dimensions</label>
+                          <div style={{ display: "flex" }}>
+                            <div>
+                              <Input placeholder="Length"></Input>
+                            </div>
+                            <div>
+                              <Input placeholder="Width"></Input>
+                            </div>
+                            <div>
+                              <Input placeholder="Height"></Input>
+                            </div>
+                          </div>
+                          <small>
+                            <u>
+                              <b>total Weight (lbs){totalWeightLBS}</b>
+                            </u>
+                          </small>
                         </Col>
                       </Row>
-                    </Card>
-                  </Col>
+                    ))}
                 </Row>
               </Container>
             </section>
